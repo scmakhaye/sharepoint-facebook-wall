3fac4f13f32dbb3aafcf2d1e9a30b6ce51afafa6
niting@brickred.com <niting@brickred.com@ecc132eb-ed4e-4443-7245-250f3795e1a1>
2010-11-16 15:38:34 +0000

    Initial Version Checked In for SharePoint 2010

---
+++ trunk/2010/BrickRed.WebParts.Facebook.Wall/Code Files/JSONObject.cs
@@ -0,0 +1,261 @@
+﻿/*
+ ===========================================================================
+ Copyright (c) 2010 BrickRed Technologies Limited
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sub-license, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ===========================================================================
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Script.Serialization;
+
+namespace BrickRed.WebParts.Facebook.Wall
+{
+    /// <summary>
+    /// Represents an object encoded in JSON. Can be either a dictionary 
+    /// mapping strings to other objects, an array of objects, or a single 
+    /// object, which represents a scalar.
+    /// </summary>
+    public class JSONObject
+    {
+        /// <summary>
+        /// Creates a JSONObject by parsing a string.
+        /// This is the only correct way to create a JSONObject.
+        /// </summary>
+        public static JSONObject CreateFromString(string s)
+        {
+            object o;
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            try
+            {
+                o = js.DeserializeObject(s);
+            }
+            catch (ArgumentException)
+            {
+                throw new Exception("JSONException", new Exception("Not a valid JSON string."));
+            }
+
+            return Create(o);
+        }
+
+        /// <summary>
+        /// Returns true if this JSONObject represents a dictionary.
+        /// </summary>
+        public bool IsDictionary
+        {
+            get
+            {
+                return _dictData != null;
+            }
+        }
+
+        /// <summary>
+        /// Returns true if this JSONObject represents an array.
+        /// </summary>
+        public bool IsArray
+        {
+            get
+            {
+                return _arrayData != null;
+            }
+        }
+
+        /// <summary>
+        /// Returns true if this JSONObject represents a string value. 
+        /// </summary>
+        public bool IsString
+        {
+            get
+            {
+                return _stringData != null;
+            }
+        }
+
+        /// <summary>
+        /// Returns true if this JSONObject represents an integer value.
+        /// </summary>
+        public bool IsInteger
+        {
+            get
+            {
+                Int64 tmp;
+                return Int64.TryParse(_stringData, out tmp);
+            }
+        }
+
+        /// <summary>
+        /// Returns true if this JSONOBject represents a boolean value.
+        /// </summary>
+        public bool IsBoolean
+        {
+            get
+            {
+                bool tmp;
+                return bool.TryParse(_stringData, out tmp);
+            }
+        }
+
+        /// <summary>
+        /// Returns this JSONObject as a dictionary
+        /// </summary>
+        public Dictionary<string, JSONObject> Dictionary
+        {
+            get
+            {
+                return _dictData;
+            }
+        }
+
+        /// <summary>
+        /// Returns this JSONObject as an array
+        /// </summary>
+        public JSONObject[] Array
+        {
+            get
+            {
+                return _arrayData;
+            }
+        }
+
+        /// <summary>
+        /// Returns this JSONObject as a string
+        /// </summary>
+        public string String
+        {
+            get
+            {
+                return _stringData;
+            }
+        }
+
+        /// <summary>
+        /// Returns this JSONObject as an integer
+        /// </summary>
+        public Int64 Integer
+        {
+            get
+            {
+                return Convert.ToInt64(_stringData);
+            }
+        }
+
+        /// <summary>
+        /// Returns this JSONObject as a boolean
+        /// </summary>
+        public bool Boolean
+        {
+            get
+            {
+                return Convert.ToBoolean(_stringData);
+            }
+        }
+
+
+        /// <summary>
+        /// Prints the JSONObject as a formatted string, suitable for viewing.
+        /// </summary>
+        public string ToDisplayableString()
+        {
+            StringBuilder sb = new StringBuilder();
+            RecursiveObjectToString(this, sb, 0);
+            return sb.ToString();
+        }
+
+        #region Private Members
+
+        private string _stringData;
+        private JSONObject[] _arrayData;
+        private Dictionary<string, JSONObject> _dictData;
+
+        private JSONObject()
+        { }
+
+        /// <summary>
+        /// Recursively constructs this JSONObject 
+        /// </summary>
+        private static JSONObject Create(object o)
+        {
+            JSONObject obj = new JSONObject();
+            if (o is object[])
+            {
+                object[] objArray = o as object[];
+                obj._arrayData = new JSONObject[objArray.Length];
+                for (int i = 0; i < obj._arrayData.Length; ++i)
+                {
+                    obj._arrayData[i] = Create(objArray[i]);
+                }
+            }
+            else if (o is Dictionary<string, object>)
+            {
+                obj._dictData = new Dictionary<string, JSONObject>();
+                Dictionary<string, object> dict =
+                    o as Dictionary<string, object>;
+                foreach (string key in dict.Keys)
+                {
+                    obj._dictData[key] = Create(dict[key]);
+                }
+            }
+            else if (o != null) // o is a scalar
+            {
+                obj._stringData = o.ToString();
+            }
+
+            return obj;
+        }
+
+        private static void RecursiveObjectToString(JSONObject obj,
+            StringBuilder sb, int level)
+        {
+            if (obj.IsDictionary)
+            {
+                sb.AppendLine();
+                RecursiveDictionaryToString(obj, sb, level + 1);
+            }
+            else if (obj.IsArray)
+            {
+                foreach (JSONObject o in obj.Array)
+                {
+                    RecursiveObjectToString(o, sb, level);
+                    sb.AppendLine();
+                }
+            }
+            else // some sort of scalar value
+            {
+                sb.Append(obj.String);
+            }
+        }
+        private static void RecursiveDictionaryToString(JSONObject obj,
+            StringBuilder sb, int level)
+        {
+            foreach (KeyValuePair<string, JSONObject> kvp in obj.Dictionary)
+            {
+                sb.Append(' ', level * 2);
+                sb.Append(kvp.Key);
+                sb.Append(" => ");
+                RecursiveObjectToString(kvp.Value, sb, level);
+                sb.AppendLine();
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ trunk/2010/BrickRed.WebParts.Facebook.Wall/ShowWall/ShowWall.cs
@@ -0,0 +1,227 @@
+﻿/*
+ ===========================================================================
+ Copyright (c) 2010 BrickRed Technologies Limited
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sub-license, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ===========================================================================
+ */
+using System;
+using System.ComponentModel;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.WebControls;
+using Microsoft.SharePoint.WebPartPages;
+using System.Net;
+using System.IO;
+
+namespace BrickRed.WebParts.Facebook.Wall
+{
+    [ToolboxItemAttribute(false)]
+    public class ShowWall : Microsoft.SharePoint.WebPartPages.WebPart
+    {
+        Label LblMessage;
+
+        #region Webpart Properties
+        [WebBrowsable(true),
+       Category("Facebook Settings"),
+       Personalizable(PersonalizationScope.Shared),
+        WebPartStorage(Storage.Shared),
+       DefaultValue(""),
+       WebDisplayName("User Id / User Name"),
+       WebDescription("Please enter user id")]
+
+        public string UserID { get; set; }
+
+
+        [WebBrowsable(true),
+       Category("Facebook Settings"),
+       Personalizable(PersonalizationScope.Shared),
+        WebPartStorage(Storage.Shared),
+       DefaultValue("10"),
+       WebDisplayName("Wall Count"),
+       WebDescription("Please enter number of wall you want to display")]
+
+        public int WallCount { get; set; }
+
+        [WebBrowsable(true),
+       Category("Facebook Settings"),
+       Personalizable(PersonalizationScope.Shared),
+        WebPartStorage(Storage.Shared),
+       DefaultValue("true"),
+       WebDisplayName("Show Description"),
+       WebDescription("Would you like to show description")]
+
+        public bool EnableShowDesc { get; set; }
+
+        #endregion
+
+        protected override void CreateChildControls()
+        {
+            base.CreateChildControls();
+            int i = 0;
+            try
+            {
+                Table mainTable;
+                TableRow tr;
+                TableCell tc;
+                mainTable = new Table();
+                mainTable.Width = Unit.Percentage(100);
+                mainTable.CellSpacing = 0;
+                mainTable.CellPadding = 0;
+
+                mainTable.CssClass = "ms-viewlsts";
+                this.Controls.Add(mainTable);
+
+                // Get an access token in some manner.
+                // By default you can only get public info.
+
+                if (!String.IsNullOrEmpty(this.UserID))
+                {
+                    JSONObject me = GetFeeds();
+                    if (me.Dictionary["data"] != null)
+                    {
+                        JSONObject[] feeds = me.Dictionary["data"].Array;
+
+                        foreach (JSONObject feed in feeds)
+                        {
+                            if (i < this.WallCount)
+                            {
+                                tr = new TableRow();
+                                mainTable.Rows.Add(tr);
+                                tc = new TableCell();
+                                tc.CssClass = "ms-vb2";
+                                tc.Width = Unit.Percentage(30);
+                                tc.Text = feed.Dictionary["message"].String;
+                                tr.Cells.Add(tc);
+
+                                if (i % 2 != 0)
+                                {
+                                    tr.CssClass = "ms-alternatingstrong";
+                                }
+
+                                tr = new TableRow();
+                                tc = new TableCell();
+                                tc.CssClass = "ms-vb2";
+
+                                if (this.EnableShowDesc)
+                                {
+                                    // tc3.VerticalAlign = VerticalAlign.Top;
+                                    mainTable.Rows.Add(tr);
+                                    tc.Text = relativeTime(feed.Dictionary["created_time"].String.ToString());
+                                    tc.Style.Add("color", "Gray");
+                                    tr.Cells.Add(tc);
+                                    if (i % 2 != 0)
+                                    {
+                                        tr.CssClass = "ms-alternatingstrong";
+                                    }
+                                }
+
+                            }
+                            else
+                            {
+                                break;
+                            }
+                            i++;
+                        }
+                    }
+                }
+                else
+                {
+                    throw new Exception("User ID / User Name  missing in webpart properties.");
+                }
+            }
+            catch (Exception Ex)
+            {
+                LblMessage = new Label();
+                LblMessage.Text = Ex.Message;
+                this.Controls.Add(LblMessage);
+            }
+        }
+
+        private string relativeTime(string pastTime)
+        {
+            DateTime origStamp = DateTime.Parse(pastTime.ToString());
+            DateTime curDate = DateTime.Now;
+
+            TimeSpan ts = curDate.Subtract(origStamp);
+            string strReturn = string.Empty;
+
+            if (ts.Days >= 1)
+            {
+                strReturn = String.Format("{0:hh:mm tt MMM dd}" + "th", Convert.ToDateTime(pastTime).ToUniversalTime());
+            }
+            else
+            {
+                if (ts.Hours >= 1)
+                    strReturn = "about " + ts.Hours + " hours ago";
+                else
+                {
+                    if (ts.Minutes >= 1)
+                    {
+                        strReturn = "about " + ts.Minutes + " minutes ago";
+                    }
+                    else
+                        strReturn = "about " + ts.Seconds + " seconds ago";
+                }
+            }
+            return strReturn;
+        }
+
+        protected override void OnPreRender(EventArgs e)
+        {
+            base.OnPreRender(e);
+        }
+
+        private JSONObject GetFeeds()
+        {
+            JSONObject obj = null;
+            string url;
+            HttpWebRequest request;
+            string oAuthToken = string.Empty;
+
+            try
+            {
+
+                url = string.Format("http://graph.facebook.com/{0}/feed", this.UserID);
+                request = WebRequest.Create(url) as HttpWebRequest;
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+                    StreamReader reader = new StreamReader(response.GetResponseStream());
+                    string retVal = reader.ReadToEnd();
+
+                    obj = JSONObject.CreateFromString(retVal);
+                    if (obj.IsDictionary && obj.Dictionary.ContainsKey("error"))
+                    {
+                        throw new Exception(obj.Dictionary["error"].Dictionary["type"].String, new Exception(obj.Dictionary["error"].Dictionary["message"].String));
+                    }
+                }
+            }
+            catch (Exception Ex)
+            {
+                LblMessage = new Label();
+                LblMessage.Text = Ex.Message;
+                this.Controls.Add(LblMessage);
+            }
+            return obj;
+        }
+    }
+}
